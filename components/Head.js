b593e36816b404975c0cdc68681bde0469c99762
KatotenMk2 <cino1031@gmail.com>
2022-06-21 21:08:50 +0900

    Fixed by Katoten

--- components/Head.js
+++ components/Head.js
@@ -7,22 +7,22 @@ export default function HeadComponent() {
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="theme-color" content="#000000" />
 
-      <title>Solana Pay</title>
-      <meta name="title" content="Solana Pay" />
-      <meta name="description" content="Buy items on my store using Solana Pay!" />
+      <title>Create your Solana Online Store</title>
+      <meta name="title" content="Create your Solana Online Store" />
+      <meta name="description" content="React.js + Next.js + Vercel 👉 Solanaでオリジナルのデータショップを作ろう✨" />
 
       {/* Facebook */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="#" />
-      <meta property="og:title" content="Solana Pay" />
-      <meta property="og:description" content="Buy items on my store using Solana Pay!" />
+      <meta property="og:url" content="https://www.shiftbase.xyz/" />
+      <meta property="og:title" content="Create your Solana Online Store" />
+      <meta property="og:description" content="React.js + Next.js + Vercel 👉 Solanaでオリジナルのデータショップを作ろう✨" />
       <meta property="og:image" content="#" />
 
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="#" />
-      <meta property="twitter:title" content="Solana Pay" />
-      <meta property="twitter:description" content="Buy items on my store using Solana Pay!" />
+      <meta property="twitter:url" content="https://www.shiftbase.xyz/" />
+      <meta property="twitter:title" content="Create your Solana Online Store" />
+      <meta property="twitter:description" content="React.js + Next.js + Vercel 👉 Solanaでオリジナルのデータショップを作ろう✨" />
       <meta property="twitter:image" content="#" />
     </Head>
   );
