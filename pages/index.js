90e8f51ee0cc0588e6d89625ebb4f629ec83ec57
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-04-23 15:15:43 +0900

    feat: add starter

---
+++ pages/index.js
@@ -0,0 +1,43 @@
+import HeadComponent from '../components/Head';
+
+const TWITTER_HANDLE = 'kii_bmi_N_perm';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+  return (
+    <div className="App">
+      <HeadComponent />
+      <div className="container">
+        <header className="header-container">
+          <p className="header"> 😳 UNCHAIN Image Store 😈</p>
+          <p className="sub-text">
+            The only Image store that accepts shitcoins
+          </p>
+        </header>
+
+        <main>
+          <img
+            src="https://media.giphy.com/media/FWAcpJsFT9mvrv0e7a/giphy.gif"
+            alt="anya"
+          />
+        </main>
+
+        <div className="footer-container">
+          <img
+            alt="Twitter Logo"
+            className="twitter-logo"
+            src="twitter-logo.svg"
+          />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built on @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
